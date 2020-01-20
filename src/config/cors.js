8d85a95086ecbdb7633120582a4bd65721326bf9
Vitor Campos <vitorp.ext@vr.com.br>
2020-01-20 11:12:51 -0300

    ajuste clima schema

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,8 @@
 module.exports = function(req, res, next) {
- res.setHeader("Access-Control-Allow-Origin", "*");
- res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
- res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Credentials', false);
  next()
 }
